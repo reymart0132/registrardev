67fc51baa26ff0aa58f6c4a89ceb5c21f962eb21
reymart0132 <35267857+reymart0132@users.noreply.github.com>
2019-10-24 15:30:19 +0800

    register done

--- resource/php/functions/funct.php
+++ resource/php/functions/funct.php
@@ -10,4 +10,96 @@ function CheckSuccess($status){
     }
 }
 
+function Success(){
+    echo '<div class="alert alert-success alert-dismissible fade show col-12" role="alert">
+            <b>Congratulations!</b> You have successfully registered a new Student Records Assistant!
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+        </div>';
+    }
+
+function pError($error){
+    echo '<div class="alert alert-danger alert-dismissible fade show col-12" role="alert">
+            <b>Error!</b> '.$error.'
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+        </div>';
+    }
+
+function vald(){
+     if(input::exists()){
+      if(Token::check(Input::get('Token'))){
+         if(!empty($_POST['College'])){
+             $_POST['College'] = implode(',',input::get('College'));
+         }else{
+            $_POST['College'] ="";
+         }
+        $validate = new Validate;
+        $validate = $validate->check($_POST,array(
+            'username'=>array(
+                'required'=>'true',
+                'min'=>4,
+                'max'=>20,
+                'unique'=>'tbl_accounts'
+            ),
+            'password'=>array(
+                'required'=>'true',
+                'min'=>6,
+            ),
+            'ConfirmPassword'=>array(
+                'required'=>'true',
+                'matches'=>'password'
+            ),
+            'fullName'=>array(
+                'required'=>'true',
+                'min'=>2,
+                'max'=>50,
+            ),
+            'College'=>array(
+                'required'=>'true'
+            )));
+
+            if($validate->passed()){
+                $user = new User();
+                $salt = Hash::salt(32);
+                try {
+                    $user->create(array(
+                        'username'=>input::get('username'),
+                        'password'=>Hash::make(input::get('password'),$salt),
+                        'salt'=>$salt,
+                        'name'=> input::get('fullName'),
+                        'joined'=>date('Y-m-d H:i:s'),
+                        'groups'=>1,
+                        'colleges'=> input::get('College')
+                    ));
+
+                    $user->createC(array(
+                        'checker'=> input::get('fullName'),
+
+                    ));
+                    $user->createV(array(
+                        'verifier'=> input::get('fullName'),
+                    ));
+
+                    $user->createR(array(
+                        'releasedby'=> input::get('fullName'),
+
+                    ));
+                } catch (Exception $e) {
+                    die($e->getMessage());
+                }
+
+                Success();
+            }else{
+                foreach ($validate->errors()as $error) {
+                pError($error);
+                }
+            }
+        }
+    }else{
+        return false;
+    }
+}
  ?>
