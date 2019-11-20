d39a7cf61ee544e2e09b333c76f225cf95026b3b
Ludwig Cartel <cartel.ludwig@gmail.com>
2019-11-20 23:23:46 +0800

    tbc

---
+++ requestRecords.php
@@ -0,0 +1,229 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Registrar Portal</title>
+  <link rel="stylesheet" type="text/css"  href="vendor/css/bootstrap.min.css">
+  <link href="vendor/css/all.css" rel="stylesheet">
+  <link rel="stylesheet" type="text/css"  href="resource/css/styles.css">
+    <link rel="stylesheet" type="text/css"  href="resource/css/record.css">
+  <link rel="stylesheet" type="text/css"  href="vendor/css/bootstrap-select.min.css">
+
+</head>
+<body>
+  <nav class="navbar navbar-dark bg-white shadow-sm slide-in-left">
+    <a class="navbar-brand" href="https://malolos.ceu.edu.ph/">
+      <img src="resource/img/logo.jpg" height="70" class="d-inline-block align-top"
+        alt="mdb logo"><h3 class="ib">
+    </a>
+       <a href="pending.php"><i class="fas fa-home ceucolor"></i></a>
+       <a href="https:/www.facebook.com/theCEUofficial/"><i class="fab fa-facebook-f ceucolor"></i></a>
+       <a href="https://www.instagram.com/ceuofficial/"><i class="fab fa-instagram ceucolor"></i></a>
+       <a href="https://twitter.com/ceumalolos"><i class="fab fa-twitter ceucolor"></i></a>
+  </nav>
+  <div class="container mt-4 puff-in-center">
+    <div class="record-con">
+      <div class="column1 text-center">
+        <h4>CENTRO ESCOLAR UNIVERSITY</h4>
+        <h4 class="ceubranch">Manila * Makati * Malolos</h4>
+        <h4>REQUEST FOR RECORDS</h4>
+      </div>
+      <!--  -->
+      <div class="column2">
+        <div class="row no-gutters">
+          <div class="col-sm-2 label">
+            <div class="data ml-1">
+              <h6>Student No.</h6>
+              <p>test</p>
+            </div>
+          </div>
+          <div class="col-sm-2 ">
+            <div class="data ml-1">
+              <h6>Last Name</h6>
+              <p>test</p>
+            </div>
+          </div>
+          <div class="col-sm-3 ">
+            <div class="data ml-1">
+              <h6>First Name</h6>
+              <p>test</p>
+            </div>
+          </div>
+          <div class="col-sm-1 label">
+            <div class="data ml-1">
+              <h6>M.I.</h6>
+              <p>test</p>
+            </div>
+          </div>
+          <div class="col-sm-2 label">
+            <div class="data ml-1">
+              <h6>Signature</h6>
+              <p>test</p>
+            </div>
+          </div>
+          <div class="col-sm-2">
+            <div class="data ml-1">
+              <h6>Date</h6>
+              <p>test</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!--  -->
+      <div class="column3">
+        <div class="row">
+          <div class="col-sm-3 label p-0">
+            <div class="data ml-1">
+              <h6>Course</h6>
+              <p>test</p>
+            </div>
+          </div>
+          <div class="col-sm-5 label p-0">
+            <div class="data ml-1">
+              <h6>Year Graduated/Year Last Enrolled</h6>
+              <p>test</p>
+            </div>
+          </div>
+          <div class="col-sm-2 label p-0">
+            <div class="data ml-1">
+              <h6>Contact No.</h6>
+              <p>test</p>
+            </div>
+          </div>
+          <div class="col-sm-2 p-0">
+            <div class="data ml-1">
+              <h6>Due Date</h6>
+              <p>test</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!--  -->
+      <div class="column4">
+        <div class="row">
+          <div class="col-sm-6 label4 p-0">
+            <h6 class="ml-1">Records to be requested</h6>
+          </div>
+          <div class="col-sm-6 p-0">
+            <h6 class="ml-1">Purpose of Request</h6>
+          </div>
+        </div>
+      </div>
+      <!--  -->
+      <div class="column5">
+        <div class="row">
+          <div class="col-sm-6 label5 p-0 pt-2">
+            <h6 class="ml-1 text-center">Sample Text</h6>
+            <h6 class="ml-1 text-center">Sample Text</h6>
+            <h6 class="ml-1 text-center">Sample Text</h6>
+            <h6 class="ml-1 text-center">Sample Text</h6>
+            <h6 class="ml-1 text-center">Sample Text</h6>
+          </div>
+          <div class="col-sm-6 p-0 text-center pt-2">
+            <h6 class="ml-1">Sample Text</h6>
+            <h6 class="ml-1">Sample Text</h6>
+            <h6 class="ml-1">Sample Text</h6>
+            <h6 class="ml-1">Sample Text</h6>
+            <h6 class="ml-1">Sample Text</h6>
+          </div>
+        </div>
+      </div>
+      <!--  -->
+      <div class="column6">
+        <div class="row">
+          <div class="col-sm-4 label6 p-0">
+            <h6 class="ml-1">Remarks</h6>
+            <p class="text-center">Sample Text</p>
+          </div>
+          <div class="col-sm-4 label6 p-0">
+            <h6 class="ml-1">For authorized representative only</h6>
+            <p class="relation ml-1">Relation:</p>
+            <p class="sp1 ml-5">Sample Text</p>
+            <hr class="hr1">
+            <p class="sp3 ml-5">Signature over printed name</p>
+          </div>
+          <div class="col-sm-4 p-0">
+            <h6 class="l3 ml-1">Accounting Department Section</h6>
+            <p class="sp1 ml-5">Sample Text</p>
+            <hr class="hr1">
+            <p class="sp2 ml-1">Accounting in-charge</p>
+          </div>
+        </div>
+      </div>
+      <!--  -->
+      <div class="column7">
+        <div class="row">
+          <h5 class="l7 ml-1">Copy to:<b>OUR/Registrar</b></h5>
+        </div>
+      </div>
+      <!--  -->
+      <div class="column8">
+        <div class="row">
+          <div class="col-sm-12 p-0">
+            <h6 class="cl8text ml-1">Sample Text</h6>
+            <p class="cl8text ml-1">Sample Date</p>
+          </div>
+        </div>
+      </div>
+      <!--  -->
+      <div class="column9">
+        <div class="row">
+          <div class="col-sm-4 p-0 label9">
+            <p class="mb-0 cl9text ml-1"><b>Note:</b> Pls. Present this stub</p>
+            <p class="mb-0 cl9text2">together with this receipt</p>
+          </div>
+          <div class="col-sm-4 p-0 label9">
+            <h5 class="ceu text-center">CENTRO ESCOLAR UNIVERSITY</h5>
+            <h5 class="ceu text-center mb-1">Manila * Makati * Malolos</h5>
+          </div>
+          <div class="col-sm-4 p-0">
+            <h5 class="ceu text-center pt-2">CLAIM STUB</h5>
+          </div>
+        </div>
+      </div>
+      <!--  -->
+      <div class="column10">
+        <div class="row">
+          <div class="col-sm-7 p-0">
+            <p class="cl8text ml-1"><b>Due Date:</b> Sample Text</p>
+            <p class="cl8text ml-1"><b>Documents requested:</b> Sample Text</p>
+          </div>
+          <div class="col-sm-5 p-0">
+
+          <p class="sp10">Sample Text</p>
+            <hr>
+            <p class="sp2 ml-1">Student Records Assistant-in-charge</p>
+          </div>
+        </div>
+      </div>
+      <!--  -->
+      <div class="column11">
+        <div class="row">
+          <div class="col-sm-12 text-center">
+            <h5>NOTE: Documents not claimed after NINETY(90) DAYS will be discarded</h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+<footer id="sticky-footer" class="py-4 bg-dark text-white-50 fixed-bottom  slide-in-right">
+  <div class="container text-center">
+      <div class="row">
+          <div class="col col-sm-5 text-left">
+              <small>Copyright &copy;Centro Escolar University     Office of the Registrar 2019</small>
+          </div>
+          <div class="col text-right">
+              <small>Created by: Reymart Bolasoc, Amelia Valencia , James Mangalile, Kenneth De Leon , Pamela Reyes , Ellen Mijares</small>
+          </div>
+      </div>
+  </div>
+</footer>
+    <script src="vendor/js/jquery.js"></script>
+    <script src="vendor/js/popper.js"></script>
+    <script src="vendor/js/bootstrap.min.js"></script>
+    <script src="vendor/js/bootstrap-select.min.js"></script>
+</body>
+</html>
